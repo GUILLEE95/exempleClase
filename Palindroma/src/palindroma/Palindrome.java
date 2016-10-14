a76fe44549e4a3edfed9b94a7f10f69e139a4a0c
alumne <alumne@alumne-VirtualBox>
2016-10-14 17:57:38 +0200

    palindromo 1.5 clase acabada

--- Palindroma/src/palindroma/Palindrome.java
+++ Palindroma/src/palindroma/Palindrome.java
@@ -10,5 +10,57 @@ package palindroma;
  * @author alumne
  */
 public class Palindrome {
+    //Declare
+    private String cadena;
+    private String aux;
+    //Constructors
+
+    public Palindrome() {
+    }
+
+    public Palindrome(String cadena, String aux) {
+        this.cadena = cadena;
+        this.aux = aux;
+    }
+
     
+    
+    //Getters and Setters
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public void setCadena(String Cadena) {
+        this.cadena = Cadena;
+    }
+
+    public String getAux() {
+        return aux;
+    }
+    
+    //Functions
+    
+    /**
+     * Comprueba si una cadena es o palindrome
+     */
+    public int palindrome() { 
+	int k=0;
+
+	
+        for (int i=this.cadena.length()-1;i>=0;i--){
+            aux =aux+cadena.charAt(i);
+        }
+        if (aux.equals(cadena)){
+            return 1;
+        }
+        else {
+            return 0;
+        }
+	
+    }
+    @Override
+    public String toString (){
+        return "La palabra introducida es :"+cadena+" La inversa es :"+aux;
+    }
 }
