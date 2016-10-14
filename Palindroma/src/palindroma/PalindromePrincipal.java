dfc57401bf153df6cbc311e82ab686fb8304e424
alumne <alumne@alumne-VirtualBox>
2016-10-14 17:41:20 +0200

    Palindroma 1.4 Principal

--- Palindroma/src/palindroma/PalindromePrincipal.java
+++ Palindroma/src/palindroma/PalindromePrincipal.java
@@ -5,6 +5,8 @@
  */
 package palindroma;
 
+import java.util.Scanner;
+
 /**
  *Nos permite identicar una cadena palindroma
  * @author guillem
@@ -15,10 +17,18 @@ public class PalindromePrincipal {
     /**
      * @param args the command line arguments
      */
+    
+    static Scanner lector = new Scanner (System.in);
+    static Palindrome palabra=new Palindrome();
+    
     public static void main(String[] args) {
         // TODO code application logic here
-        //Clase ClasePalindroma - funcion verificar_Palindroma 
+        //Clase Palindrome.java
+        //funcion verificar_Palindroma 
         //- return 1 (correcto) return 0 (incorrecto)s
+        
+       System.out.println("Dame tus numeros:");
+       palabra.setCadena(lector.next());
     }
     
 }
