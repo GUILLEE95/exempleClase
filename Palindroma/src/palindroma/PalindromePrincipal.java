116a69ee7896803884abb28c94395664cf879cdd
alumne <alumne@alumne-VirtualBox>
2016-10-18 15:54:30 +0200

    Palindroma Funcionando comentado principal sin test

--- Palindroma/src/palindroma/PalindromePrincipal.java
+++ Palindroma/src/palindroma/PalindromePrincipal.java
@@ -5,39 +5,31 @@
  */
 package palindroma;
 
-import java.util.Scanner;
+import java.util.Scanner; // Import para usar el scanner
 
 /**
- *Nos permite identicar una cadena palindroma
- * @author guillem
+ *Programa que nos permite identicar una cadena palindroma
+ * @author Guillem
  * @author Jeickson
  */
 public class PalindromePrincipal {
 
-    /**
-     * @param args the command line arguments
-     */
-    
-    static Scanner lector = new Scanner (System.in);
-    static Palindrome palabra=new Palindrome();
+    static Scanner lector = new Scanner (System.in); //Scanner per llegir per pantalla
+    static Palindrome palabra=new Palindrome(); //Clase palindroma de nombre palabra.
     
     public static void main(String[] args) {
-        // TODO code application logic here
-        //Clase Palindrome.java
-        //funcion verificar_Palindroma 
-        //- return 1 (correcto) return 0 (incorrecto)s
-      
-       System.out.println("Pon la palabra:");
-       palabra.setCadena(lector.nextLine());
-       if (palabra.verificar())
+     
+       System.out.println("Pon la palabra:"); //Printamos por pantalla para pedir la palabra
+       palabra.setCadena(lector.nextLine()); //Usamos el Scanner para leer lo que el usuario pone
+       if (palabra.verificar()) //Si la palabra devuelve true al ser palindroma
        {
-           System.out.println("La palabra es palindroma");
+           System.out.println("La palabra es palindroma"); //Printamos que es palindroma
        }
-       else
+       else //Sino es palindroma devuelve false
        {
-           System.out.println("La palabra no es palindroma");
+           System.out.println("La palabra no es palindroma");//Printamos que no es palindroma
        }
-       System.out.println(palabra.toString());
+       System.out.println(palabra.toString()); //Printa la palabra y la palabra al reves
     }
     
 }
