bbb54b852861b4d24394399e546a504b327afc79
alumne <alumne@alumne-VirtualBox>
2016-10-18 16:05:46 +0200

    palindromo final comentada con test vacio

---
+++ Palindroma/test/PalindromeTest/PalindromeTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PalindromeTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+/**
+ *
+ * @author alumne
+ */
+public class PalindromeTest {
+    
+    public PalindromeTest() {
+    }
+    
+}
